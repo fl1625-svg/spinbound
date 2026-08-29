b6d7910f67c3c31070c10147368edd4f5281e0a4
fl1625-svg <fl1625@gmail.com>
2026-08-29 16:52:26 +0800

    feat: add deterministic rotor math helpers

--- Assets/SPINBOUND/Core/Runtime/Simulation/RotorMath.cs
+++ Assets/SPINBOUND/Core/Runtime/Simulation/RotorMath.cs
@@ -19,5 +19,28 @@ namespace Spinbound.Core.Simulation
 
             return Vector2.Normalize(value);
         }
+
+        public static float WrapDeg(float degrees)
+        {
+            var wrapped = degrees % 360f;
+            return wrapped < 0f ? wrapped + 360f : wrapped;
+        }
+
+        public static Vector2 MoveToward(Vector2 current, Vector2 target, float maxDelta)
+        {
+            if (maxDelta <= 0f)
+            {
+                return current;
+            }
+
+            var delta = target - current;
+            var distance = delta.Length();
+            if (distance <= maxDelta || distance <= float.Epsilon)
+            {
+                return target;
+            }
+
+            return current + delta / distance * maxDelta;
+        }
     }
 }
